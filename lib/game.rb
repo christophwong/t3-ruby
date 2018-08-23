ce339a34c810ef9d75fd49ea6439eda309e23d3d
Christoph Wong <wong.christoph@gmail.com>
2018-08-22 21:26:52 -0700

    game calls displayBoard

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,15 @@
 class Game
-	def initialize(ui)
-	  @ui = ui
-	end
+  def initialize(ui, board)
+    @ui = ui
+    @board = board
+  end
 
-	def start
-		@ui.give("Welcome to Tic-Tac-Toe, you are 'X', play to beat 'O'", @ui)
-	end
+  def start
+    welcome
+    @board.displayBoard(@ui)
+  end
+
+  def welcome
+    @ui.give("Welcome to Tic-Tac-Toe, you are 'X', play to beat 'O'", @ui)
+  end
 end
--- test/test_game.rb
+++ test/test_game.rb
@@ -4,12 +4,15 @@ require_relative '../lib/game.rb'
 class TestGame < Minitest::Test
   def setup
   	@ui = Minitest::Mock.new
-  	@game = Game.new(@ui)
+    @board = Minitest::Mock.new
+  	@game = Game.new(@ui, @board)
   end
 
-  def test_start_game_welcomes_player
+  def test_start_game_welcomes_player_and_display_board
     @ui.expect(:give, true, [/Welcome/, @ui])
+    @board.expect(:displayBoard, true, [@ui])
     @game.start
     @ui.verify
+    @board.verify
   end
 end
