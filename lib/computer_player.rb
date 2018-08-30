c901e44b5d290f729ebef071f7bd03e6d231005f
Christoph Wong <wong.christoph@gmail.com>
2018-08-30 11:25:53 -0700

    basic minmax algorithm implemented, needs to connect other UI aspects

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,11 +1,125 @@
 class ComputerPlayer
+  def initialize
+    @mark = "O"
+  end
+
   def get_chosen_index(board)
+    available_spaces = get_available_spaces(board)
+
+    if available_spaces.one?
+      return available_spaces.first
+    end
+    # and return their end state ranking (10, 0, -10)
+    # if it's computer's turn, we want the index with ranking 10
+    # because that means computer is winning, yay AI overlords
+    # but 0 means no win or losses...
+    #
+
+    ranks_and_i = ranks_and_index(available_spaces, board)
+    select_index_from_rank_pair ranks_and_i.max
+
+  end
+
+  def ranks_and_index(available_spaces, board)
+    ranks = []
+    computer_players_turn = true
+    available_spaces.each do |i|
+      next_board = board.dup
+      next_board[i] = @mark
+      rank = get_ranking(next_board, computer_players_turn)
+      # puts "ranks is : #{rank} and i #{i}"
+      ranks << [rank, i]
+    end
+
+    ranks
+  end
+
+  def select_index_from_rank_pair(rank)
+    rank[1]
+  end
+
+  def get_ranking(board, computer_players_turn, depth = 0)
+  winner = get_winner(board)
+    if winner == "O"
+      return (10 - depth)
+    elsif winner == "X"
+      return (-10 + depth)
+    elsif no_more_available_move?(board)
+      return (0 - depth)
+    else
+      depth += 1
+      computer_players_turn = !computer_players_turn
+      mark = computer_players_turn ? "O" : "X"
+      #question: filling first box seems wrong. Shall we fill with minmax strategy? Yes! i think so
+      next_board = fill_first_available_box(board, mark)
+
+      return get_ranking(next_board, computer_players_turn, depth)
+    end
+  end
+
+  def fill_first_available_box(board, mark)
+    i = get_available_spaces(board).first
+    new_board = board.dup
+    new_board[i] = mark
+    new_board
+  end
+
+  def no_more_available_move?(board)
+    !board.include?(nil)
+  end
+
+  def get_available_spaces(board)
     available_spaces = []
-    board.each_with_index {|cell, i|
+    board.each_with_index do |cell, i|
       if cell.nil?
         available_spaces << i
       end
-    }
-    available_spaces.first
+    end
+    available_spaces
+  end
+
+  def get_winner(board)
+    check_combos(get_all_combos(board))
+  end
+
+  def check_combos(combos)
+    return "X" if check_win_for(combos, "X")
+    return "O" if check_win_for(combos, "O")
+  end
+
+  def check_win_for(combos, mark)
+    combos.include?(Array.new(3, mark))
+  end
+
+  def get_all_combos(board)
+    rows = get_row(board)
+    columns = get_col(rows)
+    diagonals = get_diag(rows)
+    [*rows, *columns, *diagonals]
+  end
+
+  def get_diag(rows)
+    top_left_to_bottom_right = []
+    top_right_to_bottom_left = []
+
+    rows.each_with_index do |row, index|
+      top_left_to_bottom_right << row[index]
+      top_right_to_bottom_left << row[2 - index]
+    end
+
+    return [top_left_to_bottom_right, top_right_to_bottom_left]
+  end
+
+  def get_col(rows)
+    rows.transpose
+  end
+
+  def get_row(board)
+    rows = []
+    board = board.dup
+    3.times do |i|
+      rows << board.shift(3)
+    end
+    rows
   end
 end
--- test/computer_player_test.rb
+++ test/computer_player_test.rb
@@ -15,11 +15,76 @@ class TestComputerPlayer < Minitest::Test
   end
 
   def test_cp_chose_winning_move
-    skip("build minmax alg first")
-    board = [nil, nil, "O",
-             nil, nil, nil,
-             nil, nil, "O"]
-
+    board = [
+      nil, nil, "O",
+      nil, nil, nil,
+      nil, nil, "O"
+    ]
     assert_equal 5, @computer_player.get_chosen_index(board)
   end
+
+  def test_cp_block_opponents_win
+    board = [
+      "O", "X", "X",
+      nil, "X", "O",
+      nil, "O", "X"
+    ]
+    assert_equal 6, @computer_player.get_chosen_index(board)
+  end
+
+  def test_cp_gets_10_rank_for_win
+    board = [
+      "O", "O", "O",
+      nil, nil, nil,
+      nil, nil, nil
+    ]
+
+    assert_equal 10, @computer_player.get_ranking(board, true)
+  end
+
+
+  def test_cp_gets_negative_ten_rank_for_loss
+    board = [
+      "X", "X", "X",
+      nil, nil, nil,
+      nil, nil, nil
+    ]
+    assert_equal (-10), @computer_player.get_ranking(board, true)
+  end
+
+  def test_get_winner_returns_winner
+    assert_equal "X", @computer_player.get_winner(["X", "X", "X", nil, nil, nil, nil, nil, nil])
+    assert_equal "O", @computer_player.get_winner(["O", "O", "O", nil, nil, nil, nil, nil, nil])
+    assert_nil @computer_player.get_winner(["X", "O", "X", nil, nil, nil, nil, nil, nil])
+  end
+
+  def test_board_is_full
+    assert @computer_player.no_more_available_move?(["X", "O"])
+    refute @computer_player.no_more_available_move?([nil, "X", "O"])
+  end
+
+  def test_ranks_and_index_gives_10
+    board = [
+      "X", "X", nil,
+      nil, "O", "X",
+      "X", "X", "O"
+    ]
+    available_spaces = [2, 3]
+    ranks_and_index = @computer_player.ranks_and_index(available_spaces, board)
+    assert_equal [[-9, 2],[-9, 3]], ranks_and_index
+  end
+
+  def test_fill_first_available_box
+    board = [
+     "X", "X", nil,
+     nil, "O", "X",
+     "X", "X", "O"
+   ]
+
+   next_board = board.dup
+   next_board[2] = "X"
+
+   result = @computer_player.fill_first_available_box(board, "X")
+   assert_equal next_board, result
+ end
 end
